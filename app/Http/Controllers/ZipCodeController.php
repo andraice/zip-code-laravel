abb53b7ad978593865c8d84ac3f1a03ad12ab0bf
Alberto.Sanchez <alberto.sanchez@parclick.com>
2022-08-10 02:13:06 -0500

    add select columns

--- app/Http/Controllers/ZipCodeController.php
+++ app/Http/Controllers/ZipCodeController.php
@@ -10,22 +10,28 @@ class ZipCodeController extends Controller
     public function getZipCodes(String $zip_code)
     {
         try {
-            $zipCodesList = ZipCode::where('d_codigo', $zip_code)->get();
+            $zipCodesList = ZipCode::select('d_ciudad', 'c_estado', 'd_estado', 'c_CP', 'c_estado', 'd_mnpio', 'id_asenta_cpcons', 'd_asenta', 'd_zona', 'd_tipo_asenta')->where('d_codigo', $zip_code)->get();
 
             if (isset($zipCodesList)) {
-                $firstItem = $zipCodesList->first();
-                $locality = $firstItem['d_ciudad'];
 
-                $federal_entity = array(
-                    "key" => (int) $firstItem['c_estado'],
-                    "name" => $firstItem['d_estado'],
-                    "code" => !isset($firstItem['c_CP']) ? null : $firstItem['c_CP']
-                );
-                $municipality = array(
-                    "key" => (int) $firstItem['c_mnpio'],
-                    "name" => $firstItem['d_mnpio']
-                );
+                // extract only first
+                foreach ($zipCodesList as $item) {
+                    $locality = $item['d_ciudad'];
 
+                    $federal_entity = array(
+                        "key" => (int) $item['c_estado'],
+                        "name" => $item['d_estado'],
+                        "code" => !isset($item['c_CP']) ? null : $item['c_CP']
+                    );
+                    $municipality = array(
+                        "key" => (int) $item['c_mnpio'],
+                        "name" => $item['d_mnpio']
+                    );
+
+                    break;
+                }
+
+                // extract all settlements
                 foreach ($zipCodesList as $item) {
                     $settlements[] = array(
                         "key" => (int) $item['id_asenta_cpcons'],
