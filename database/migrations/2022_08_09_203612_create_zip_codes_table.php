5ac12a7b5251254a410dc7ad5aa5feece39ca7a5
Alberto.Sanchez <alberto.sanchez@parclick.com>
2022-08-09 19:37:37 -0500

    add innodb

--- database/migrations/2022_08_09_203612_create_zip_codes_table.php
+++ database/migrations/2022_08_09_203612_create_zip_codes_table.php
@@ -13,8 +13,24 @@ return new class extends Migration
      */
     public function up()
     {
+        
         Schema::create('zip_codes', function (Blueprint $table) {
             $table->id();
+            $table->integer('d_codigo');
+            $table->string('d_asenta');
+            $table->string('d_tipo_asenta');
+            $table->string('d_mnpio');
+            $table->string('d_estado');
+            $table->string('d_ciudad');
+            $table->integer('d_CP');
+            $table->integer('c_estado');
+            $table->integer('c_oficina');
+            $table->string('c_CP');
+            $table->integer('c_tipo_asneta');
+            $table->integer('c_mnpio');
+            $table->integer('id_asenta_cpcons');
+            $table->string('d_zona');
+            $table->integer('c_cve_ciudad');
             $table->timestamps();
         });
     }
